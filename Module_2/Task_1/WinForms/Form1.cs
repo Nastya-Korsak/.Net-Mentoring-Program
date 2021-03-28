f455e321bcce83a0afbb550ec2c4241e928de612
Anastasiya Korsak <k-n05@mail.ru>
2021-03-28 17:50:59 +0300

    Task 1

---
+++ Module_2/Task_1/WinForms/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var name = this.textBox1.Text;
+
+            MessageBox.Show($"Hello {name}!");
+        }
+    }
+}
