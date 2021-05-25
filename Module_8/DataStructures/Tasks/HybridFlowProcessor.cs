814fd0325faeed5e516f2e8122b3d6d1ec0a8880
Anastasiya Korsak <k-n05@mail.ru>
2021-05-25 22:21:11 +0300

    add second task

--- Module_8/DataStructures/Tasks/HybridFlowProcessor.cs
+++ Module_8/DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,41 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private readonly DoublyLinkedList<T> _itemsList;
+
+        public HybridFlowProcessor()
+        {
+            _itemsList = new DoublyLinkedList<T>();
+        }
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (_itemsList.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _itemsList.RemoveAt(0);
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _itemsList.Add(item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (_itemsList.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _itemsList.RemoveAt(_itemsList.Length - 1);
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _itemsList.Add(item);
         }
     }
 }
