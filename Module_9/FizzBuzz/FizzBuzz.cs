433a6ad9241c6d0d0e6e22c02901b68ed2fe691e
Anastasiya Korsak <k-n05@mail.ru>
2021-06-01 23:12:22 +0300

    Add isNumberDivisibleBy3 and isNumberDivisibleBy5 methods

--- Module_9/FizzBuzz/FizzBuzz.cs
+++ Module_9/FizzBuzz/FizzBuzz.cs
@@ -14,5 +14,15 @@ namespace FizzBuzz
         {
             throw new NotImplementedException();
         }
+
+        private bool isNumberDivisibleBy3(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        private bool isNumberDivisibleBy5(int number)
+        {
+            return number % 5 == 0;
+        }
     }
 }
