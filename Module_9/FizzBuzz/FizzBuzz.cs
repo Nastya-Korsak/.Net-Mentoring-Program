1d48bb7eb3ef3c490cb83111b38a54e218e9bff4
Anastasiya Korsak <k-n05@mail.ru>
2021-06-01 23:08:18 +0300

    Generate NumberHandler and GetNumbers methods

--- Module_9/FizzBuzz/FizzBuzz.cs
+++ Module_9/FizzBuzz/FizzBuzz.cs
@@ -1,6 +1,18 @@
-﻿namespace FizzBuzz
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        public string NumberHandler(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetNumbers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
