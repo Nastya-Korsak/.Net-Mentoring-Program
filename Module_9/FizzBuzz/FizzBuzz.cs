5f33187e121ae5d9aee90479bd2334107fe3e51d
Anastasiya Korsak <k-n05@mail.ru>
2021-06-01 23:22:15 +0300

    Add implementation of NumberHandler method

--- Module_9/FizzBuzz/FizzBuzz.cs
+++ Module_9/FizzBuzz/FizzBuzz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace FizzBuzz
 {
@@ -7,7 +8,29 @@ namespace FizzBuzz
     {
         public string NumberHandler(int number)
         {
-            throw new NotImplementedException();
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+
+            var returnedValue = new StringBuilder();
+
+            if (isNumberDivisibleBy3(number))
+            {
+                returnedValue.Append("Fizz");
+            }
+
+            if (isNumberDivisibleBy5(number))
+            {
+                returnedValue.Append("Buzz");
+            }
+
+            if (returnedValue.Length == 0)
+            {
+                return number.ToString();
+            }
+
+            return returnedValue.ToString();
         }
 
         public List<string> GetNumbers()
